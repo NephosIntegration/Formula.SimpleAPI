a6e1cee65a5ccd8aa700b4020d4b88b5957ec42a
Joshua Williams <joshua.p.williams@gmail.com>
2020-01-19 22:23:51 -0600

    Added Get method with delegate for defaults

--- Services/CorsConfigLoader.cs
+++ Services/CorsConfigLoader.cs
@@ -1,57 +1,24 @@
 using System;
 using System.IO;
 using System.Text.Json;
+using Formula.Core;
 
 namespace Formula.SimpleAPI
 {
-    public class CorsConfigLoader : ICorsConfig
+    public class CorsConfigLoader : ConfigLoader<CorsConfigDefinition>, ICorsConfig
     {
-        protected CorsConfigDefinition instance = null;
-
-        public CorsConfigLoader LoadFromFile(String fileName)
-        {
-            var json = File.ReadAllText(fileName);
-            this.instance = JsonSerializer.Deserialize<CorsConfigDefinition>(json);
-            return this;
-        }
-
-        public CorsConfigLoader SaveToFile(String fileName)
-        {
-            if (this.InstanceValid())
-            {
-                var json = JsonSerializer.Serialize(this.instance);
-                var fileStream = File.Open(fileName, FileMode.Append, FileAccess.Write);
-                var fileWriter = new StreamWriter(fileStream);
-                fileWriter.Write(json);
-                fileWriter.Flush();
-                fileWriter.Close();
-            }
-
-            return this;
-        }
-
-        protected Boolean InstanceValid(Boolean throwIfNot = true)
-        {
-            Boolean output = false;
-
-            if (this.instance == null)
-            {
-                if (throwIfNot)
-                {
-                    throw new Exception("Cors Configuration not found");
-                }
-            }
-            else
-            {
-                output = true;
-            }
-
-            return output;
-        }
-
         public string[] GetOrigins()
         {
             return this.instance.Origins;
+        }
+
+        public static new CorsConfigLoader Get(String fileName, GetDefaults getDefaults = null)
+        {
+            var output = new CorsConfigLoader();
+
+            output.LoadFromFile(fileName, getDefaults);
+
+            return output;
         }
 
     }

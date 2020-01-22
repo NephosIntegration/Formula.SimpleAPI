3ed6d3899f727caf36972ba7987d5a3ea2b7b20a
Joshua Williams <joshua.p.williams@gmail.com>
2020-01-22 01:11:54 -0600

    Added proper resource based routes

--- Base/ResourceControllerBase.cs
+++ Base/ResourceControllerBase.cs
@@ -24,24 +24,57 @@ namespace Formula.SimpleAPI
             _repository = repository;
         }
 
+        [HttpGet("query/{constraints}")]
+        public virtual Task<IEnumerable<TModel>> Query(String constraints)
+        {
+            var bindable = _repository.WhereFromJson(constraints);
+            return _repository.GetListAsync(bindable.Sql, bindable.Parameters);
+        }
+
+        // Gets a specific resource by id
+        [HttpGet("{id}")]
+        public virtual Task<TModel> Get(int id) 
+        {
+            return _repository.GetAsync(id);
+        }
+
+        // Gets all resources
         [HttpGet]
         public virtual Task<IEnumerable<TModel>> GetList()
         {
             return _repository.GetListAsync();
         }
 
-        [HttpGet("{id}")]
-        public Task<TModel> Get(int id) 
+        // Create a new resource
+        [HttpPost]
+        public virtual async Task<TModel> Post(TModel model)
         {
-            return _repository.GetAsync(id);
+            var id = await _repository.InsertAsync(model);
+            return await _repository.GetAsync(id);
         }
 
-        [HttpGet("query/{constraints}")]
-        public Task<IEnumerable<TModel>> Query(String constraints)
+        // Updates the resource identified by id
+        [HttpPut("{id}")]
+        public virtual async Task<TModel> Put(int id, TModel model)
         {
-            var bindable = _repository.WhereFromJson(constraints);
-            return _repository.GetListAsync(bindable.Sql, bindable.Parameters);
+            var recordsUpdated = await _repository.UpdateAsync(model);
+            return await _repository.GetAsync(id);
         }
 
+        // Update a specific attribute on a resource
+        [HttpPatch("{id}")]
+        public virtual async Task<TModel> Patch(int id, PatchModel model)
+        {
+            throw new Exception("Not implemented yet");
+            return await _repository.GetAsync(id);
+        }
+
+        // Delete removes resource
+        [HttpDelete("{id}")]
+        public virtual async Task<TModel> Delete(int id, TModel model)
+        {
+            var recordsUpdated = await _repository.DeleteAsync(model);
+            return await _repository.GetAsync(id);
+        }
     }
 }
---
+++ Models/PatchModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Formula.SimpleAPI
+{
+    public class PatchModel
+    {
+        public String Field = null;
+        public String Value = null;
+    }
+}
